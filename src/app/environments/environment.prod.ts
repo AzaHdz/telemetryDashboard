95c081771299957819002d0479d7b7a7642b8199
Azael Hernandez <ahernandez@maxipublica.com>
2025-04-30 11:04:53 -0600

    update api prod

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://18.227.48.34/api', // URL del backend en producción
+  apiUrl: 'http://18.227.48.34:3000/api', // URL del backend en producción
 };
